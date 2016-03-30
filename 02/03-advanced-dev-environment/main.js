2fe69664627aeb4edd1ee1f57b3dcfe7127377c4
Susanna Kosic <susanna.kosic@gmail.com>
2016-03-30 23:16:53 +0200

    Formatting

--- 02/03-advanced-dev-environment/main.js
+++ 02/03-advanced-dev-environment/main.js
@@ -36,9 +36,9 @@ var App = React.createClass({
                 <Summary date={this.state.date} calories={this.state.calories} />
                 <Controls />
                 <EntryTable
-                date={this.state.date}
-                entries={this.state.entries}
-                deleteEntry={this.deleteEntry} />
+                    date={this.state.date}
+                    entries={this.state.entries}
+                    deleteEntry={this.deleteEntry} />
             </section>
             )
     }
@@ -52,7 +52,7 @@ var Summary = React.createClass({
                 <div className="calories">{this.props.calories}</div>
                 <div className="label">calories left</div>
             </h1>
-            )
+        )
     }
 });
 
@@ -66,7 +66,7 @@ var Controls = React.createClass({
                 <li><a className="button" href="./exercise">Exercise</a></li>
                 <li><a className="button" href="./more">More</a></li>
             </ul>
-            )
+        )
     }
 });
 
@@ -78,7 +78,7 @@ var EntryTable = React.createClass({
             index={key}
             entry={this.props.entries[key]}
             deleteEntry={this.props.deleteEntry} />
-            )
+        )
     },
     render: function() {
         return (
@@ -99,7 +99,7 @@ var EntryTable = React.createClass({
                     </tbody>
                 </table>
             </section>
-            )
+        )
     }
 });
 
@@ -133,7 +133,7 @@ var EntryTableRow = React.createClass({
                     onClick={this.handleClick}>Delete</a>
                 </td>
             </tr>
-            )
+        )
     }
 });
 
